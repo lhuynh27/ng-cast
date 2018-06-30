3d8924be4b1505019d412ed76919d97432936118
lhuynh27 <leannehuynh27@gmail.com>
2018-06-29 17:35:02 -0700

    Implementing onClick

--- src/components/app.js
+++ src/components/app.js
@@ -1,5 +1,8 @@
 angular.module('video-player')
-
 .component('app', {
-  // TODO
+  controller: function() {
+    this.videos = window.exampleVideoData;
+    this.index = 0;
+  },
+  templateUrl: 'src/templates/app.html'
 });
--- src/components/videoListEntry.js
+++ src/components/videoListEntry.js
@@ -1,4 +1,10 @@
 angular.module('video-player')
 .component('videoListEntry', {
-  // TODO
+  bindings: {
+    video: '<'
+  },
+  controller: function() {
+    console.log(this);
+  },
+  templateUrl: 'src/templates/videoListEntry.html'
 });
